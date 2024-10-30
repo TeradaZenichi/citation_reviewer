826bdb7f7af0ace6516c3f2a775f6f92c6e05e07
Lucas <lucaszenichi@outlook.com>
2024-10-29 23:28:01 -0300

    improve crossref check function

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ paper = papers[0]
 text = paper["metadata"]["sections"]           # Texto completo do paper que vamos analisar
 references = paper["metadata"]["references"]   # dicionário de referências do paper
 
-# references, report = check.fill(references)
+references, report = check.fill(references)
 
-# #convert report dict to DataFrame
-# report_df = pd.DataFrame(report)
+#convert report dict to DataFrame
+report_df = pd.DataFrame(report)
 
-# # save the report to a csv file - transposed
-# report_df.T.to_csv("report.csv")
+# save the report to a csv file - transposed
+report_df.T.to_csv("report.csv")
 
 data = sentences.extract_sentences_with_citations(text)
 
